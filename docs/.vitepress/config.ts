3f367b07d4ff0034b1132c559a01eed909b11c3c
Bjorn-Granstrom <granis92@gmail.com>
2024-03-08 17:36:20 +0100

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: 'src',
-  base: '/Klinikstatus/',
+  base: '/klinikstatus/',
   title: "Klinikstatus",
   description: "Yes",
   themeConfig: {
